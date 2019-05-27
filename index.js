ed84966c933ea2631133a7506313a34cc591fd9c
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-05-27 15:28:49 -0400

    Added button and function to move through array

--- index.js
+++ index.js
@@ -10,10 +10,30 @@ var prompts = [
 //change alert to display the 0th element of the array, 'Type your name'
 //comment out all alerts
 //alert(prompts[0]);
-
 //alert(prompts.length);//--> 3
 //alert(prompts[1]);//-> 'Type an adjective'
 //alert(prompts[0]);//'Type your name'
 
+//keep track on the current prompt that we're on
+//this value represent the index number of of the array, prompts
+var currentPrompt = 0;
+
+//fucntion that will call the next prompt
+var nextPrompt = function(){
 //jQuery html() method to return the strings as innerHTML of the div element with class = "prompts". 
-$('.prompt').html(prompts[2]);
+//change content in bracket to variable, 'currentPrompt',now it is dynamic
+
+$('.prompt').html(prompts[currentPrompt]);
+
+
+//make the number held inside currentPrompt be one value larger than the number inside currentPrompt
+currentPrompt = currentPrompt + 1;
+
+}
+
+
+//run prompt function when button is clicked
+
+$('button').click(function() {
+  nextPrompt();
+});
